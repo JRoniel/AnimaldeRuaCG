01bb48e41ca8dfaf14ae285cd6b4385ca5575c43
johnroniel8264@gmail.com <johnroniel8264@gmail.com>
2023-09-29 14:06:37 -0300

    Iniciando repo com base da homepage

---
+++ assets/scripts/main.js
@@ -0,0 +1,28 @@
+// Script para tornar o menu responsivo
+
+function activeMenu() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+    // Adicionar classe 'change' para animar o ícone de hambúrguer
+    var iconBar = document.querySelector(".icon-bar");
+    iconBar.classList.toggle("change");
+}
+
+function toggleZoom(image) {
+    var images = document.querySelectorAll(".square-image"); // Obtém todas as imagens
+
+    // Remove a classe de zoom e adiciona a classe de desfoque em todas as imagens
+    for (var i = 0; i < images.length; i++) {
+        if (images[i] !== image) {
+            images[i].classList.remove("zoomed");
+            images[i].classList.add("blurred");
+        }
+    }
+
+    // Adiciona a classe de zoom na imagem clicada
+    image.classList.toggle("zoomed");
+}
