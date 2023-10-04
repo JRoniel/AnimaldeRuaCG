59b93ca9fcd5064b9a19f000741bcbf7a08bccbd
John Roniel <60148238+JRoniel@users.noreply.github.com>
2023-10-04 18:39:11 -0300

    refatoração de codigo javascript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,5 @@
-document.querySelector(".logo").addEventListener("click", ()=> {
-  window.location.href="..,index.html"
-});
+let domain = 'https://animalderuacg.vercel.app/';
 
-function activeMenu() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-        // Adicione a classe 'hide' para esconder a logo quando o menu está aberto
-        document.querySelector(".logo").classList.add("hide");
-    } else {
-        x.className = "navbar";
-        // Remova a classe 'hide' para mostrar a logo quando o menu é recolhido
-        document.querySelector(".logo").classList.remove("hide");
-    }
-    // Adicionar classe 'change' para animar o ícone de hambúrguer
-    var iconBar = document.querySelector(".icon-bar");
-    iconBar.classList.toggle("change");
-}
-
-// Função para alternar o zoom das imagens
-function toggleZoom(image) {
-    var images = document.querySelectorAll(".square-image");
-
-    // Remover a classe de zoom e adicionar a classe de desfoque em todas as imagens, exceto na imagem clicada
-    images.forEach(function (img) {
-        if (img !== image) {
-            img.classList.remove("zoomed");
-            img.classList.add("blurred");
-        }
-    });
-
-    // Alternar a classe de zoom na imagem clicada
-    image.classList.toggle("zoomed");
-}
-
-// Configuração para exibição do aviso de cookies
 let cookieConfig = {
     title: "Aviso de Cookies 🍪",
     description: "Este site utiliza cookies para garantir que você obtenha a melhor experiência possível. Ao continuar a navegar neste site, você concorda com o uso de cookies. Para mais informações consulte nossa Política de Privacidade e Cookies.",
@@ -42,76 +7,105 @@ let cookieConfig = {
     button: "Entendi 👍"
 };
 
-function fadeIn(element, display) {
-    var el = document.getElementById(element);
-    el.style.opacity = 0;
-    el.style.display = display || "block";
+class Main {
+    constructor() {
+        this.initEvents();
+        this.cookieConsent();
+    }
 
-    (function fade() {
-        var opacity = parseFloat(el.style.opacity);
-        if ((opacity += 0.02) > 1) {
-            return;
-        }
-        el.style.opacity = opacity;
-        requestAnimationFrame(fade);
-    })();
-}
+    initEvents = () => {
+        document.querySelector(".logo").addEventListener("click", () => {
+            window.location.href = domain;
+        });
+        // Event Listeners aqui
+    }
 
-function fadeOut(element) {
-    var el = document.getElementById(element);
-    el.style.opacity = 1;
-
-    (function fade() {
-        if ((el.style.opacity -= 0.02) < 0) {
-            el.style.display = "none";
+    activeMenu() {
+        var x = document.getElementById("myNavbar");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+            // Adicione a classe 'hide' para esconder a logo quando o menu está aberto
+            document.querySelector(".logo").classList.add("hide");
         } else {
+            x.className = "navbar";
+            // Remova a classe 'hide' para mostrar a logo quando o menu é recolhido
+            document.querySelector(".logo").classList.remove("hide");
+        }
+        // Adicionar classe 'change' para animar o ícone de hambúrguer
+        var iconBar = document.querySelector(".icon-bar");
+        iconBar.classList.toggle("change");
+    }
+
+    fadeIn(element, display) {
+        var el = document.getElementById(element);
+        el.style.opacity = 0;
+        el.style.display = display || "block";
+
+        (function fade() {
+            var opacity = parseFloat(el.style.opacity);
+            if ((opacity += 0.02) > 1) {
+                return;
+            }
+            el.style.opacity = opacity;
             requestAnimationFrame(fade);
-        }
-    })();
-}
-
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+        })();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
-function getCookie(name) {
-    var cookieName = name + "=";
-    var cookieArray = document.cookie.split(";");
-    for (var i = 0; i < cookieArray.length; i++) {
-        var cookie = cookieArray[i];
-        while (cookie.charAt(0) === " ") {
-            cookie = cookie.substring(1, cookie.length);
+    fadeOut(element) {
+        var el = document.getElementById(element);
+        el.style.opacity = 1;
+
+        (function fade() {
+            if ((el.style.opacity -= 0.02) < 0) {
+                el.style.display = "none";
+            } else {
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
         }
-        if (cookie.indexOf(cookieName) === 0) {
-            return cookie.substring(cookieName.length, cookie.length);
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    getCookie(name) {
+        var cookieName = name + "=";
+        var cookieArray = document.cookie.split(";");
+        for (var i = 0; i < cookieArray.length; i++) {
+            var cookie = cookieArray[i];
+            while (cookie.charAt(0) === " ") {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
+            }
+        }
+        return null;
+    }
+
+    cookieConsent = () => {
+        if (!this.getCookie("purecookieDismiss")) {
+            document.body.innerHTML +=
+                '<div id="cookieConsentContainer" class="cookieConsentContainer">' +
+                '<div class="cookieTitle"><a>' + cookieConfig.title + '</a></div>' +
+                '<div class="cookieDesc"><p>' + cookieConfig.description + " " + cookieConfig.link + '</p></div>' +
+                '<div class="cookieButton"><a onClick="mainInstance.purecookieDismiss();">' + cookieConfig.button + '</a></div>' +
+                '</div>';
+            this.fadeIn("cookieConsentContainer");
         }
     }
-    return null;
-}
 
-function cookieConsent() {
-    if (!getCookie("purecookieDismiss")) {
-        document.body.innerHTML +=
-            '<div id="cookieConsentContainer" class="cookieConsentContainer">' +
-            '<div class="cookieTitle"><a>' + cookieConfig.title + '</a></div>' +
-            '<div class="cookieDesc"><p>' + cookieConfig.description + " " + cookieConfig.link + '</p></div>' +
-            '<div class="cookieButton"><a onClick="purecookieDismiss();">' + cookieConfig.button + '</a></div>' +
-            '</div>';
-        fadeIn("cookieConsentContainer");
+    purecookieDismiss = () => {
+        this.setCookie("purecookieDismiss", "1", 7);
+        this.fadeOut("cookieConsentContainer");
     }
+
 }
 
-function purecookieDismiss() {
-    setCookie("purecookieDismiss", "1", 7);
-    fadeOut("cookieConsentContainer");
-}
-
-window.onload = function () {
-    cookieConsent();
-};
+const mainInstance = new Main();
