7ed40f5cbff90f5a53649c052d880bde8db748f7
johnroniel8264@gmail.com <johnroniel8264@gmail.com>
2023-10-10 13:15:08 -0300

    atualização

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,128 +1,128 @@
 let domain = 'https://animalderuacg.vercel.app/';
 
 let cookieConfig = {
-    title: "Aviso de Cookies 🍪",
-    description: "Este site utiliza cookies para garantir que você obtenha a melhor experiência possível. Ao continuar a navegar neste site, você concorda com o uso de cookies. Para mais informações consulte nossa Política de Privacidade e Cookies.",
-    link: '<a href="/pages/privacy-policy.html" target="_blank">Para que serve?</a>',
-    button: "Entendi 👍"
+  title: "Aviso de Cookies 🍪",
+  description: "Este site utiliza cookies para garantir que você obtenha a melhor experiência possível. Ao continuar a navegar neste site, você concorda com o uso de cookies. Para mais informações consulte nossa Política de Privacidade e Cookies.",
+  link: '<a href="/pages/privacy-policy.html" target="_blank">Para que serve?</a>',
+  button: "Entendi 👍"
 };
 
 class Main {
-    constructor() {
-        this.initEvents();
-        this.cookieConsent();
-        this.correctYear();
-    }
+  constructor() {
+    this.initEvents();
+    this.cookieConsent();
+    this.correctYear();
+  }
 
-    initEvents() {
-        document.querySelector(".logo").addEventListener("click", () => {
-            window.location.href = domain;
-        });
-    }
-    
-    openFullscreen(element) {
-  const imageSrc = element.src;
-  document.getElementById('fullscreen-img').src = imageSrc;
-  document.getElementById('fullscreen').style.display = 'block';
-}
+  initEvents() {
+    document.querySelector(".logo").addEventListener("click", () => {
+      window.location.href = domain;
+    });
+  }
+
+  openFullscreen(element) {
+    const imageSrc = element.src;
+    document.getElementById('fullscreen-img').src = imageSrc;
+    document.getElementById('fullscreen').style.display = 'block';
+  }
 
 
- closeFullscreen() {
+  closeFullscreen() {
     document.getElementById('fullscreen').style.display = 'none';
-}
+  }
 
 
-    activeMenu() {
-        const x = document.getElementById("myNavbar");
-        const logo = document.querySelector(".logo");
-        const iconBar = document.querySelector(".icon-bar");
+  activeMenu() {
+    const x = document.getElementById("myNavbar");
+    const logo = document.querySelector(".logo");
+    const iconBar = document.querySelector(".icon-bar");
 
-        if (x.className === "navbar") {
-            x.className += " responsive";
-            logo.classList.add("hide");
-        } else {
-            x.className = "navbar";
-            logo.classList.remove("hide");
-        }
-
-        iconBar.classList.toggle("change");
-    }
-    
-    correctYear(){
-      document.getElementById("year").innerText = new Date().getFullYear();
+    if (x.className === "navbar") {
+      x.className += " responsive";
+      logo.classList.add("hide");
+    } else {
+      x.className = "navbar";
+      logo.classList.remove("hide");
     }
 
-    // Restante do seu código aqui...
+    iconBar.classList.toggle("change");
+  }
 
-    fadeIn(element, display) {
-        var el = document.getElementById(element);
-        el.style.opacity = 0;
-        el.style.display = display || "block";
+  correctYear() {
+    document.getElementById("year").innerText = new Date().getFullYear();
+  }
 
-        (function fade() {
-            var opacity = parseFloat(el.style.opacity);
-            if ((opacity += 0.02) > 1) {
-                return;
-            }
-            el.style.opacity = opacity;
-            requestAnimationFrame(fade);
-        })();
+  // Restante do seu código aqui...
+
+  fadeIn(element, display) {
+    var el = document.getElementById(element);
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+
+    (function fade() {
+      var opacity = parseFloat(el.style.opacity);
+      if ((opacity += 0.02) > 1) {
+        return;
+      }
+      el.style.opacity = opacity;
+      requestAnimationFrame(fade);
+    })();
+  }
+
+  fadeOut(element) {
+    var el = document.getElementById(element);
+    el.style.opacity = 1;
+
+    (function fade() {
+      if ((el.style.opacity -= 0.02) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+
+  setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
     }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
 
-    fadeOut(element) {
-        var el = document.getElementById(element);
-        el.style.opacity = 1;
-
-        (function fade() {
-            if ((el.style.opacity -= 0.02) < 0) {
-                el.style.display = "none";
-            } else {
-                requestAnimationFrame(fade);
-            }
-        })();
+  getCookie(name) {
+    var cookieName = name + "=";
+    var cookieArray = document.cookie.split(";");
+    for (var i = 0; i < cookieArray.length; i++) {
+      var cookie = cookieArray[i];
+      while (cookie.charAt(0) === " ") {
+        cookie = cookie.substring(1, cookie.length);
+      }
+      if (cookie.indexOf(cookieName) === 0) {
+        return cookie.substring(cookieName.length, cookie.length);
+      }
     }
+    return null;
+  }
 
-    setCookie(name, value, days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  cookieConsent() {
+    if (!this.getCookie("purecookieDismiss")) {
+      document.body.innerHTML +=
+        '<div id="cookieConsentContainer" class="cookieConsentContainer">' +
+        '<div class="cookieTitle"><a>' + cookieConfig.title + '</a></div>' +
+        '<div class="cookieDesc"><p>' + cookieConfig.description + " " + cookieConfig.link + '</p></div>' +
+        '<div class="cookieButton"><a onClick="mainInstance.purecookieDismiss();">' + cookieConfig.button + '</a></div>' +
+        '</div>';
+      this.fadeIn("cookieConsentContainer");
     }
+  }
 
-    getCookie(name) {
-        var cookieName = name + "=";
-        var cookieArray = document.cookie.split(";");
-        for (var i = 0; i < cookieArray.length; i++) {
-            var cookie = cookieArray[i];
-            while (cookie.charAt(0) === " ") {
-                cookie = cookie.substring(1, cookie.length);
-            }
-            if (cookie.indexOf(cookieName) === 0) {
-                return cookie.substring(cookieName.length, cookie.length);
-            }
-        }
-        return null;
-    }
-
-    cookieConsent() {
-        if (!this.getCookie("purecookieDismiss")) {
-            document.body.innerHTML +=
-                '<div id="cookieConsentContainer" class="cookieConsentContainer">' +
-                '<div class="cookieTitle"><a>' + cookieConfig.title + '</a></div>' +
-                '<div class="cookieDesc"><p>' + cookieConfig.description + " " + cookieConfig.link + '</p></div>' +
-                '<div class="cookieButton"><a onClick="mainInstance.purecookieDismiss();">' + cookieConfig.button + '</a></div>' +
-                '</div>';
-            this.fadeIn("cookieConsentContainer");
-        }
-    }
-
-    purecookieDismiss() {
-        this.setCookie("purecookieDismiss", "1", 7);
-        this.fadeOut("cookieConsentContainer");
-    }
+  purecookieDismiss() {
+    this.setCookie("purecookieDismiss", "1", 7);
+    this.fadeOut("cookieConsentContainer");
+  }
 }
 
 const main = new Main();
